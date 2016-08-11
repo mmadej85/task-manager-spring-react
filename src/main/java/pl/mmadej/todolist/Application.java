cd0b175e9f3d71e130496d441db730232b2e21f1
mmadej85 <hello@maciejmadej.com>
2016-08-11 15:22:45 +0200

    test commit

--- src/main/java/pl/mmadej/todolist/Application.java
+++ src/main/java/pl/mmadej/todolist/Application.java
@@ -2,14 +2,11 @@ package pl.mmadej.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
